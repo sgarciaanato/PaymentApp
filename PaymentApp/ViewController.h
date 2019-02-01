a6477294dbef6aa3e8b8fc9169158b538c1512eb
sagarana <samuel.garciaanato@cencosud.cl>
2019-01-31 22:50:45 -0300

    Initial Commit

---
+++ PaymentApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaymentApp
+//
+//  Created by Samuel on 1/31/19.
+//  Copyright © 2019 MercadoLibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PaymentApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PaymentApp
+//
+//  Created by Samuel on 1/31/19.
+//  Copyright © 2019 MercadoLibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
